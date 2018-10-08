0953586c3e4ac1bd697883902275075650b51a6f
Wanl23 <ivan.leschinsky@yandex.ru>
2018-10-08 18:41:59 +0500

    moderated checkwin method. Now it works for any size of field

--- l2/src/ru/geekbrains/lesson3/TicTac.java
+++ l2/src/ru/geekbrains/lesson3/TicTac.java
@@ -11,9 +11,9 @@ public class TicTac {
     private static Scanner SCANNER = new Scanner(System.in);
     private static Random RANDOM = new Random();
 
-    private static int fieldSizeY = 3;
-    private static int fieldSizeX = 3;
-    private static int winRate = 3;
+    private static int fieldSizeY = 5;
+    private static int fieldSizeX = 5;
+    private static int winRate = 4;
     private static char[][] field;
 
     private static void initField(){
@@ -26,10 +26,10 @@ public class TicTac {
     }
 
     private static void printField(){
-        System.out.println(" _y_y_y");
-        for(int y = 0; y < fieldSizeY; y++){
+        System.out.println(" _y_y_y_y_y");
+        for(int y = 0; y < field.length; y++){
             System.out.print("x ");
-            for(int x = 0; x < fieldSizeX; x++){
+            for(int x = 0; x < field.length; x++){
                 System.out.print(field[y][x] + "|");
             }
             System.out.println();
@@ -56,80 +56,83 @@ public class TicTac {
     }
 
     private static void aiTurn(){
-        int counterDiag1 = 0;
-        int counterDiag2 = 0;
-        boolean moveDone = false;
-        for(int y = 0; y < field.length; y++) {
-            if(moveDone){
-                break;
-            }
-            int humanDotQtyX = 0;
-            int humanDotQtyY = 0;
-            for (int x = 0; x < field.length; x++) {
-                if(field[y][x] == HUMAN_DOT){
-                    humanDotQtyX++;
-                    if(humanDotQtyX == 2){
-                        for(int xx = 0; xx < field.length; xx++ ){
-                            if(isEmptyCell(y, xx)) {
-                                field[y][xx] = AI_DOT;
-                                moveDone = true;
-                                break;
-                            }
-                        }
-                        break;
-                    }
-                }
-                if(field[x][y] == HUMAN_DOT){
-                    humanDotQtyY++;
-                    if(humanDotQtyY == 2){
-                        for(int xx = 0; xx < field.length; xx++ ){
-                            if(isEmptyCell(xx, y)) {
-                                field[xx][y] = AI_DOT;
-                                moveDone = true;
-                                break;
-                            }
-                        }
-                        break;
-                    }
-                }
-                if (x == y && field[y][x] == HUMAN_DOT) {
-                    counterDiag1++;
-                    if(counterDiag1 == 2){
-                        for(int i = 0; i < field.length; i++ ){
-                            if(isEmptyCell(i, i)) {
-                                field[i][i] = AI_DOT;
-                                moveDone = true;
-                                break;
-                            }
-                        }
-                        break;
-                    }
-                }
-                if (x == field.length - y - 1 && field[y][x] == HUMAN_DOT) {
-                    counterDiag2++;
-                    if(counterDiag2 == 2){
-                        for(int i = 0; i < field.length; i++ ){
-                            if(isEmptyCell(field.length - i - 1, field.length - i - 1)) {
-                                field[field.length - i - 1][field.length - i - 1] = AI_DOT;
-                                moveDone = true;
-                                break;
-                            }
-                        }
-                        break;
-                    }
-                }
-            }
-            if(y == 2){
-                int i;
-                int j;
-                do {
-                    i = RANDOM.nextInt(fieldSizeX);
-                    j = RANDOM.nextInt(fieldSizeY);
-                } while (!isEmptyCell(i, j));
-                field[i][j] = AI_DOT;
-                break;
-            }
-        }
+//        int counterDiag1 = 0;
+//        int counterDiag2 = 0;
+//        boolean moveDone = false;
+//        for(int y = 0; y < field.length; y++) {
+//            if(moveDone){
+//                break;
+//            }
+//            int humanDotQtyX = 0;
+//            int humanDotQtyY = 0;
+//            for (int x = 0; x < field.length; x++) {
+//                if(field[y][x] == HUMAN_DOT){
+//                    humanDotQtyX++;
+//                    if(humanDotQtyX == 2){
+//                        for(int xx = 0; xx < field.length; xx++ ){
+//                            if(isEmptyCell(y, xx)) {
+//                                field[y][xx] = AI_DOT;
+//                                moveDone = true;
+//                                break;
+//                            }
+//                        }
+//                    }
+//                }
+//                if(field[x][y] == HUMAN_DOT){
+//                    humanDotQtyY++;
+//                    if(humanDotQtyY == 2){
+//                        for(int xx = 0; xx < field.length; xx++ ){
+//                            if(isEmptyCell(xx, y)) {
+//                                field[xx][y] = AI_DOT;
+//                                moveDone = true;
+//                                break;
+//                            }
+//                        }
+//                    }
+//                }
+//                if (x == y && field[y][x] == HUMAN_DOT) {
+//                    counterDiag1++;
+//                    if(counterDiag1 == 2){
+//                        for(int i = 0; i < field.length; i++ ){
+//                            if(isEmptyCell(i, i)) {
+//                                field[i][i] = AI_DOT;
+//                                moveDone = true;
+//                                break;
+//                            }
+//                        }
+//                    }
+//                }
+//                if (x == field.length - y - 1 && field[y][x] == HUMAN_DOT) {
+//                    counterDiag2++;
+//                    if(counterDiag2 == 2){
+//                        for(int i = 0; i < field.length; i++ ){
+//                            if(isEmptyCell(field.length - i - 1, field.length - i - 1)) {
+//                                field[field.length - i - 1][field.length - i - 1] = AI_DOT;
+//                                moveDone = true;
+//                                break;
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+//            if(y == 2){
+//                int i;
+//                int j;
+//                do {
+//                    i = RANDOM.nextInt(fieldSizeX);
+//                    j = RANDOM.nextInt(fieldSizeY);
+//                } while (!isEmptyCell(i, j));
+//                field[i][j] = AI_DOT;
+//                break;
+//            }
+//        }
+        int i;
+        int j;
+        do {
+            i = RANDOM.nextInt(fieldSizeX);
+            j = RANDOM.nextInt(fieldSizeY);
+        } while (!isEmptyCell(i, j));
+        field[i][j] = AI_DOT;
     }
 
     private static boolean isMapFull(){
@@ -143,28 +146,50 @@ public class TicTac {
         return true;
     }
 
-    private static boolean checkWin(char dot){
+    private static boolean checkWin(char dot) {
         int counterDiag1 = 0;
         int counterDiag2 = 0;
-        for(int y = 0; y < field.length; y++) {
+        int counterDiag3 = 0;
+        int counterDiag4 = 0;
+        for (int y = 0; y < field.length; y++) {
             int counterX = 0;
             int counterY = 0;
             for (int x = 0; x < field.length; x++) {
-                if (field[y][x] == dot) {
-                    counterX++;
+                if (field[y][x] == dot) counterX++;
+                if (field[x][y] == dot) counterY++;
+                if (counterX == winRate || counterY == winRate) return true;
+
+                if ((y - x) >= 0) {
+                    if (field[x][y - x] == dot) {
+                        counterDiag1++;
+                        if (counterDiag1 == winRate) return true;
+                    }
+                } else counterDiag1 = 0;
+
+                if(x > 0) {
+                    if (field[x][field.length - x] == dot) {
+                        counterDiag2++;
+                        if (counterDiag2 == winRate) return true;
+                    }
                 }
-                if (field[x][y] == dot) {
-                    counterY++;
+                else counterDiag2 = 0;
+
+                if((field.length - 1 - y + x) < field.length){
+                    if(field[x][field.length - 1 - y + x] == dot){
+                        counterDiag3++;
+                        if(counterDiag3 == winRate) return true;
+                    }
                 }
-                if (x == y && field[y][x] == dot) {
-                    counterDiag1++;
+                else counterDiag3 = 0;
+
+                if(y + x < field.length && y > 0){
+                    if(field[x+y][x] == dot){
+                        counterDiag4++;
+                        if(counterDiag4 == winRate) return true;
+                    }
                 }
-                if (x == field.length - y - 1 && field[y][x] == dot) {
-                    counterDiag2++;
-                }
+                else counterDiag4 = 0;
             }
-            if (counterX == winRate || counterY == winRate || counterDiag1 == winRate || counterDiag2 == winRate)
-                return true;
         }
         return false;
     }
